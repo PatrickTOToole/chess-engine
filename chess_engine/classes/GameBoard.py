34a6ae77d9f877a147147bb8967f161d7c3e1daa
PatrickTOToole <PatrickTOToole@gmail.com>
2022-03-17 23:07:58 -0400

    Added Mouse clicks

--- chess_engine/classes/GameBoard.py
+++ chess_engine/classes/GameBoard.py
@@ -9,33 +9,48 @@ from chess_engine.classes import Knight
 from chess_engine.util.Team import Team
 import tkinter as tk
 
+
+def toChr(num):
+    return chr(96 + num)
+
+class objWrapper:
+    def __init__(self, obj, col, row, move):
+        self.obj = obj
+        self.col = col
+        self.row = row
+        self.mov = move
+    def callback(self, event):
+        self.mov.move.append(f"{toChr(self.col)}{self.row}")
+        print("hi")
+        print(self.mov.move)
+
 class GameBoard:
-    def __init__(self, frame):
+    def __init__(self, frame, move):
         self.elts = {}
+        self.board = {}
+        self.frame = frame
         self.passant = None
+        self.move = move
         for i in range(1,9):
             tk_arr = {}
-            for j in range(1,9):
-                rank = self.toStdIdx(i) % 2
-                file = j % 2
-                color = (rank + file) % 2
-                bg_curr = "brown"
-                if color:
-                    bg_curr = "tan"
-                label = tk.Label(master=frame, text="", bg=bg_curr, width = 5, height=2)
-                label.grid(row = 8-(self.toStdIdx(j)) , column = self.toStdIdx(i))
-                tk_arr[j] = label
-            self.elts[self.toChr(i)] = tk_arr
-        self.frame = frame
-        self.board = {}
-        for i in range(1,9):
             new_arr = {}
             for j in range(1,9):
                 rank = self.toStdIdx(i) % 2
                 file = j % 2
                 color = (rank + file) % 2
                 new_arr[j] = Space(self.toChr(i), j, color, self)
+                bg_curr = "brown"
+                if color:
+                    bg_curr = "tan"
+                label = tk.Label(master=frame, text="", bg=bg_curr, width = 5, height=2)
+                wlabel = objWrapper(label, i, j, self.move)
+                wlabel.obj.callback = wlabel.callback
+                wlabel.obj.bind("<Button-1>", wlabel.obj.callback)
+                wlabel.obj.grid(row = 8-(self.toStdIdx(j)) , column = self.toStdIdx(i))
+                tk_arr[j] = wlabel
+            self.elts[self.toChr(i)] = tk_arr
             self.board[self.toChr(i)] = new_arr
+            
         for i in range(1,9):
             self.addPiece(Pawn(Team.WHITE, self.board[self.toChr(i)][2], self), i, 2)
             self.addPiece(Pawn(Team.BLACK, self.board[self.toChr(i)][7], self), i, 7)
@@ -70,7 +85,7 @@ class GameBoard:
         return num - 1
     def addPiece(self, pc, i, j):
         self.board[self.toChr(i)][j].piece = pc
-        self.elts[self.toChr(i)][j].config(text = str(pc))
+        self.elts[self.toChr(i)][j].obj.config(text = str(pc))
 
     def updateUI(self):
         for i in range(1,9):
@@ -78,7 +93,7 @@ class GameBoard:
                 pz = str(self.board[self.toChr(i)][j].piece)
                 if pz == "None":
                     pz = ""
-                self.elts[self.toChr(i)][j].config(text = pz)
+                self.elts[self.toChr(i)][j].obj.config(text = pz)
 
 
     def getPieceAt(self, notation: str):
@@ -87,14 +102,14 @@ class GameBoard:
             row = int(notation[1])
             return self.board[col][row].piece
         else:
-            raise Exception("Invalid Notation - Expected: [a-h][1-8]")
+            return None
     def getSpaceAt(self, notation: str):
         if len(notation) == 2:
             col = notation[0]
             row = int(notation[1])
             return self.board[col][row]
         else:
-            raise Exception("Invalid Notation - Expected: [a-h][1-8]")
+            return None
     def __str__(self):
         out = ""
         for i in self.board:
--- main.py
+++ main.py
@@ -1,51 +1,80 @@
+from glob import glob
+from operator import is_
 from chess_engine import *
 import tkinter as tk
 import sys
 
 window = tk.Tk()
 board_frame = tk.Frame()
-gameboard = GameBoard(board_frame)
+class moveWrapper:
+    def __init__(self):
+        self.move = []
+mov = moveWrapper()
+gameboard = GameBoard(board_frame, mov)
 board_frame.pack()
+is_white = True
 logfile = open("log", "w+")
+move_record = {1:[]}
+move_count = 1
 
-def doMove(move_count, move_record, team):
+def doMove(team, moveNot):
     res = False
     cap = False
-    while(not res):
-        if team == Team.BLACK:
-            team_str = "black"
-        else:
-            team_str = "white"
-        move = input(f"move ({team_str}): ")
-        m = move.split("-")
-        pieceRef = gameboard.getPieceAt(str(m[0]))
-        if pieceRef == None or pieceRef.team != team:
-            continue
-        target = gameboard.getSpaceAt(m[1])
-        res, cap = pieceRef.move(target)
+    global move_count
+    global move_record
+    # while(not res):
+        # if team == Team.BLACK:
+        #     team_str = "black"
+        # else:
+        #     team_str = "white"
+        #move = input(f"move ({team_str}): ")
+
+    m = moveNot.split("-")
+    pieceRef = gameboard.getPieceAt(str(m[0]))
+    if pieceRef == None or pieceRef.team != team:
+        return move_count, move_record, False
+    target = gameboard.getSpaceAt(m[1])
+    res, cap = pieceRef.move(target)
     if cap:
         mid = "x"
     else:
         mid = "-"
+    print(move_record[move_count], team == Team.BLACK)
+
     move_record[move_count].append(m[0] + mid + m[1])
     if gameboard.passant != None and gameboard.passant.passant_piece != None:
         if gameboard.passant.passant_piece.team != team:
             gameboard.passant.resetPassant()
             gameboard.passant = None
-    print(gameboard)
     if team == Team.BLACK:
         logfile.write(f"{move_count}: {move_record[move_count][0]} {move_record[move_count][1]}\n")
         move_count += 1
 
 
-    return move_count, move_record
+    return move_count, move_record, True
 
-def task(move_count, move_record):
-    move_record[move_count] = []
-    move_count, move_record = doMove(move_count, move_record, Team.WHITE)
-    move_count, move_record = doMove(move_count, move_record, Team.BLACK)
-    window.after(0, task, move_count, move_record)
+def task():
+    global mov
+    global is_white
+    global move_count
+    global move_record
+    if len(mov.move) < 2:
+        window.after(1, task)
+    else:
+        p1 = mov.move[0]
+        p2 = mov.move[1]
+        mov.move = []
+        moveNot = f"{p1}-{p2}"
+        if is_white:
+            move_count, move_record, res = doMove(Team.WHITE, moveNot)
+            is_white = False
+        else:
+            move_count, move_record, res = doMove(Team.BLACK, moveNot)
+            move_record[move_count] = []
+            is_white = True
 
-move_record = {}
-window.after(0, task, 1, move_record)
+        window.after(0, task)
+
+    
+window.after(0, task)
 window.mainloop()
